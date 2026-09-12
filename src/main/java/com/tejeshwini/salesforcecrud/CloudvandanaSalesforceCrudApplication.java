c041eab67a2ee365b96dac9f61ef8cb5cc35824a
tejuingalagi <tejuingalagi07@gmail.com>
2026-09-12 22:16:26 +0530

    Initial project setup

---
+++ src/main/java/com/tejeshwini/salesforcecrud/CloudvandanaSalesforceCrudApplication.java
@@ -0,0 +1,13 @@
+package com.tejeshwini.salesforcecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudvandanaSalesforceCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudvandanaSalesforceCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tejeshwini/salesforcecrud/CloudvandanaSalesforceCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejeshwini.salesforcecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudvandanaSalesforceCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
